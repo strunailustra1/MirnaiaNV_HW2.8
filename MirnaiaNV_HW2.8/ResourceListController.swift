1fb23d3183cf7a27668e8583eedc7989f29f1617
Natalia Mirnaia <strunailustra@gmail.com>
2019-12-08 15:16:23 +0300

    add ResourceCell class

--- MirnaiaNV_HW2.8/ResourceListController.swift
+++ MirnaiaNV_HW2.8/ResourceListController.swift
@@ -17,6 +17,9 @@ class ResourceListController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.estimatedRowHeight = 150
+        tableView.rowHeight = UITableView.automaticDimension
+        
         hideKeyboardWhenTappedAround()
     }
 }
@@ -27,9 +30,12 @@ extension ResourceListController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "resourceItem", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "resourceItem", for: indexPath) as! ResourceCell
         
-        cell.textLabel?.text = resourceList[indexPath.row].article
+        cell.articleLabel.text = resourceList[indexPath.row].article
+        cell.brandLabel.text = resourceList[indexPath.row].brand
+        cell.namelabel.text = resourceList[indexPath.row].name
+        cell.priceLabel.text = String(resourceList[indexPath.row].price)
         
         return cell
     }
