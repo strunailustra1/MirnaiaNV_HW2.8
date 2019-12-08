b678afef851b2445fb75f4382b89f9c2d1bd337e
Natalia Mirnaia <strunailustra@gmail.com>
2019-12-08 19:00:20 +0300

    work with cart

--- MirnaiaNV_HW2.8/ResourceDetailViewController.swift
+++ MirnaiaNV_HW2.8/ResourceDetailViewController.swift
@@ -7,8 +7,16 @@
 //
 
 import UIKit
+
+enum CartButtonTitle: String {
+    case add = "Положить в корзину"
+    case change = "Изменить количество"
+    case remove = "Удалить из корзины"
+}
+
 class ResourceDetailViewController: UIViewController {
     var resource: Resource!
+    
     @IBOutlet var imageResource: UIImageView!
     @IBOutlet var brandLabel: UILabel!
     @IBOutlet var articleLabel: UILabel!
@@ -17,6 +25,7 @@ class ResourceDetailViewController: UIViewController {
     @IBOutlet var quantityLabel: UILabel!
     @IBOutlet var amountLabel: UILabel!
     @IBOutlet var quantityStepper: UIStepper!
+    @IBOutlet var addToCartButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,11 +35,40 @@ class ResourceDetailViewController: UIViewController {
         nameLabel.text = resource.name
         priceLabel.text = String(resource.price)
         
+        if let cartItem = Cart.instance.getCartItems()[resource.id] {
+            quantityStepper.value = Double(cartItem.quantity)
+            addToCartButton.setTitle(CartButtonTitle.change.rawValue, for: .normal)
+        }
+        
         changeAmount()
     }
     
     @IBAction func quantityChange() {
         changeAmount()
+        changeButtonTitle()
+    }
+    
+    @IBAction func addToCart() {
+        Cart.instance.addToCart(
+            cartItem: CartItem(
+                quantity: Int(quantityStepper.value),
+                resource: resource
+            )
+        )
+        
+        changeButtonTitle()
+    }
+    
+    private func changeButtonTitle() {
+        let inCart = Cart.instance.getCartItems()[resource.id] != nil
+        
+        if inCart && quantityStepper.value > 0 {
+            addToCartButton.setTitle(CartButtonTitle.change.rawValue, for: .normal)
+        } else if quantityStepper.value > 0 {
+            addToCartButton.setTitle(CartButtonTitle.add.rawValue, for: .normal)
+        } else {
+            addToCartButton.setTitle(CartButtonTitle.remove.rawValue, for: .normal)
+        }
     }
     
     private func changeAmount() {
